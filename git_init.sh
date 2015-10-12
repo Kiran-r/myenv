b3427817fd725b323c7d0f96887745757ff134fb
Kiran Ranganath <kiran.ranganath@maplelabs.com>
2015-10-12 12:54:00 +0530

    Add shell script to configure git

---
+++ git_init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Author: Kiran Ranganath <kiran7193@gmail.com>
+
+git config --global user.name "Kiran Ranganath"
+git config --global user.email "kiran.ranganath@maplelabs.com"
+git config --global color.diff auto
+git config --global color.status auto
+git config --global color.branch auto
+git config --global color.ui auto
+git config --global difftool.prompt false
+git config --global core.editor vim
+git config --global --add gitreview.username "Kiran"
+git config --global --add gitreview.scheme "https"
+git config --global --add gitreview.port "443"
